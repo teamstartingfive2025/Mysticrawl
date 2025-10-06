5bf37e985ff0ff791d3685d78c4d0de433980173
Skimlk <vanhoutencocoapowder@gmail.com>
2025-10-06 00:08:51 -0400

    START5-47 - Display the title screen when the game is opened

--- Mysticrawl/Mysticrawl.cpp
+++ Mysticrawl/Mysticrawl.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <tuple>
+#include <functional>
+#include "Constants.h"
+#include "UI.h"
+using namespace std;
+
+void displayTitleScreen() {
+	SelectionMenu menu({
+		{
+			{"Enter Dungeon", [](){ /*Enter Dungeon*/; }},
+		},
+		{
+			{"Exit Game", [](){ exit(0); }},
+		},
+	});
+
+	while(true) {
+		cout << Constants::UI::WELCOME_MESSAGE << endl;
+		cout << Constants::Story::BACKSTORY << endl;
+
+		cout << Constants::UI::NAVIGATE_TEXT << endl;
+		cout << Constants::UI::CONTINUE_TEXT << "\n\n";
+
+		menu.MakeSelection();
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	displayTitleScreen();
+    return 0;
 }
