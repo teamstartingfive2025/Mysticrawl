5453f91f0e098ca7a2fac242bf60f7104707371a
Skimlk <vanhoutencocoapowder@gmail.com>
2025-10-06 00:06:37 -0400

    START5-47 - Create 'Constants' header to consolidate controls, UI elements, eliminate magic numbers, and store backstory

---
+++ Mysticrawl/Constants.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <string>
+#include <array>
+using namespace std;
+
+namespace Constants {
+	namespace UI {
+		//Game Title and Expressions
+		const string GAME_TITLE = "Mysticrawl";
+		const string WELCOME_MESSAGE = "Welcome to " + GAME_TITLE + "!";
+
+		//Coloring
+		const string ESCAPE_CODE = "\033[";
+		const string SELECTION_COLOR = ESCAPE_CODE + "1;31m"; // Bold Red text
+		const string COLOR_RESET = ESCAPE_CODE + "0m"; // Resets text formatting
+
+		//User Prompts
+		const string SELECT_KEY_TEXT = "Enter";
+		const int SELECT_KEY_CODE = 13;	// ASCII code for Enter key
+		const string NAVIGATE_TEXT = "Use the arrow keys to navigate the menu.";
+		const string CONTINUE_TEXT = "Press " + SELECT_KEY_TEXT + " to make a selection...";
+
+		//Menu Options
+		const int MENU_OPTION_DIMENSIONS = 2; // 2D menu
+		const int INITIAL_MENU_OPTION_INDEX = 0; // Start at the first option
+		const enum FOCUSED_INDEX_DIMENSION { ROW, COL };
+		const enum MENU_COMPONENTS { LABEL, ACTION };
+		const string FOCUSED_INDEX_PRETEXT = SELECTION_COLOR; // Bold Red text
+		const string FOCUSED_INDEX_POSTTEXT = COLOR_RESET; // Resets text formatting
+		const array<int, MENU_OPTION_DIMENSIONS> DEFAULT_FOCUSED_INDEX = { INITIAL_MENU_OPTION_INDEX, INITIAL_MENU_OPTION_INDEX };
+
+		//Menu Navigation
+		const int UP_KEY_CODE = 72;    // ASCII code for Up Arrow
+		const int DOWN_KEY_CODE = 80;  // ASCII code for Down Arrow
+		const int LEFT_KEY_CODE = 75;  // ASCII code for Left Arrow
+		const int RIGHT_KEY_CODE = 77; // ASCII code for Right Arrow
+
+		const array<int, MENU_OPTION_DIMENSIONS> UP_KEY_MODIFIER = { -1, 0 };
+		const array<int, MENU_OPTION_DIMENSIONS> DOWN_KEY_MODIFIER = { 1, 0 };
+		const array<int, MENU_OPTION_DIMENSIONS> LEFT_KEY_MODIFIER = { 0, -1 };
+		const array<int, MENU_OPTION_DIMENSIONS> RIGHT_KEY_MODIFIER = { 0, 1 };
+		
+	}
+
+	namespace Story {
+		const string BACKSTORY = R"(
+In a world where magic and mystery intertwine, you find yourself trapped in an ancient dungeon.
+Its walls echo with the whispers of forgotten souls. The air is thick with enchantment, 
+and every shadow seems to conceal a lurking danger. To escape, you must gather spellbooks, wands,
+and other magical trinkets to navigate this labyrinthine filled with treacherous creatures and cunning puzzles.
+)";
+	}
+}
