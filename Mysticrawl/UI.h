9d13b0e7aafdcba4ae6b67f7399abecccef5e11e
Skimlk <vanhoutencocoapowder@gmail.com>
2025-10-06 00:08:11 -0400

    START5-47 - Create SelectionMenu UI element

---
+++ Mysticrawl/UI.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "UI.h"
+#include "Constants.h"
+#include "Input.h"
+#include <vector>
+#include <array>
+#include <functional>
+#include <tuple>
+#include <iostream>
+
+using namespace std;
+
+class SelectionMenu {
+private:
+    vector<vector< tuple<string, function<void()>> >> options;
+    array<int, Constants::UI::MENU_OPTION_DIMENSIONS> focusedIndex;
+    array<int, Constants::UI::MENU_OPTION_DIMENSIONS> selectionIndex;
+
+    void DisplayOptions();
+    void ApplyKeyModifier(const array<int, Constants::UI::MENU_OPTION_DIMENSIONS> modifier);
+
+public:
+    SelectionMenu(const vector<vector< tuple<string, function<void()>> >>& opts);
+
+    void MakeSelection();
+};
