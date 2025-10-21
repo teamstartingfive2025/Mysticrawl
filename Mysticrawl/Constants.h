9a4fb96c4a17324af71f604e364d2322d7939fef
Skimlk <vanhoutencocoapowder@gmail.com>
2025-10-21 00:17:16 -0400

    START5-58 - Add border to selection menu

--- Mysticrawl/Constants.h
+++ Mysticrawl/Constants.h
@@ -23,9 +23,11 @@ namespace Constants {
 		//Menu Options
 		const int MENU_OPTION_DIMENSIONS = 2; // 2D menu
 		const int INITIAL_MENU_OPTION_INDEX = 0; // Start at the first option
+		const int VALUE_UNSET = -1;
 		const enum FOCUSED_INDEX_DIMENSION { ROW, COL };
 		const enum MENU_COMPONENTS { LABEL, ACTION };
 		const string FOCUSED_INDEX_PRETEXT = SELECTION_COLOR; // Bold Red text
+		const int MENU_OPTION_PADDING_TABS = 2;
 		const string FOCUSED_INDEX_POSTTEXT = COLOR_RESET; // Resets text formatting
 		const array<int, MENU_OPTION_DIMENSIONS> DEFAULT_FOCUSED_INDEX = { INITIAL_MENU_OPTION_INDEX, INITIAL_MENU_OPTION_INDEX };
 
@@ -34,6 +36,12 @@ namespace Constants {
 		const int DOWN_KEY_CODE = 80;  // ASCII code for Down Arrow
 		const int LEFT_KEY_CODE = 75;  // ASCII code for Left Arrow
 		const int RIGHT_KEY_CODE = 77; // ASCII code for Right Arrow
+
+		//Menu Styling
+		const int TAB_SIZE = 8;
+		const int MENU_ITEMS_PER_ROW = 2;
+		const string MENU_VERTICAL_BORDER = "|";
+		const string MENU_HORIZONTAL_BORDER = "=";
 
 		const array<int, MENU_OPTION_DIMENSIONS> UP_KEY_MODIFIER = { -1, 0 };
 		const array<int, MENU_OPTION_DIMENSIONS> DOWN_KEY_MODIFIER = { 1, 0 };
