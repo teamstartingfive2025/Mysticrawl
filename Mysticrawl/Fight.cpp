fcfafa1008f44a2d6a08a1d37380c3aee2fa9216
pmaraldo817 <mrpmaraldo@gmail.com>
2025-10-24 14:35:29 -0400

    START5-60 Outlined what functions necessary for story

--- Mysticrawl/Fight.cpp
+++ Mysticrawl/Fight.cpp
@@ -2,8 +2,14 @@
 #include "Player.h"
 #include "Enemy.h"
 #include "Dungeon.h"
+#include <iostream>
+using namespace std;
 
 void Fight::fightMenu(Player& player) {
+    // 1. Retrieve a reference to the current room the player is standing in
+    // 2. Validate that the room actually contains enemies
+    // 3. Create our menu option list using lambdas.
+    // 4. Display the fight menu and handle input
     vector<tuple<string, function<void()>>> fightOptions;
 
     fightOptions = {
--- Mysticrawl/Fight.h
+++ Mysticrawl/Fight.h
@@ -5,8 +5,13 @@
 #include "Interactable.h"
 #include "Player.h"
 
+// The Fight class handles all combat-related player actions.
+// For this story, we’re only implementing the player's basic attack.
 class Fight : public Interactable {
 public:
+	// Displays a simple fight menu and allows the player to attack an enemy.
+	// Parameters:
+		//player: reference to the player object (attacker)
 	Fight() {}
-	void fightMenu(Player& player);
+	void fightMenu(Player& player, Room& room); 
 };
