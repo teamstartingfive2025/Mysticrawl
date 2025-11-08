536c4a89fc2c3d63a2746180e9cc0c64dada851a
Skimlk <vanhoutencocoapowder@gmail.com>
2025-11-07 22:22:50 -0500

    START5-74 - Resolve multiple delcaration error

---
+++ Mysticrawl/EnemyTemplates.cpp
@@ -0,0 +1,10 @@
+#include "EnemyTemplates.h"
+
+Enemy RatTemplate(
+    "Rat", "A rat suddenly appears! I hope it doesn't have rabies...\n",
+    5,  // hit points
+    20, // block chance (%)
+    1,  // min damage
+    3,  // max damage
+    40  // block exit chance (%)
+);
--- Mysticrawl/EnemyTemplates.h
+++ Mysticrawl/EnemyTemplates.h
@@ -1,12 +1,4 @@
 #pragma once
 #include "Enemy.h"
 
-class Enemy;
-
-extern Enemy RatTemplate = Enemy("Rat", "A rat suddenly appears! I hope it doesn't have rabies...\n", 
-	5, //hit points
-	20,//block chance (%)
-	1, //min damage
-	3, //max damage
-	40 //block exit chance (%)
-);
+extern Enemy RatTemplate;
