2bdee7c8423bfde1b827a8f0aa74f992cf375836
Skimlk <vanhoutencocoapowder@gmail.com>
2025-11-13 15:57:43 -0500

    START5-67 - Move Exit locking logic into Lockable class

--- Mysticrawl/Exit.h
+++ Mysticrawl/Exit.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Room.h"
+#include "Lockable.h"
 #include <string>
 #include <tuple>
 #include <functional>
@@ -13,19 +14,14 @@ using namespace std;
  * Each exit contains a direction (Ex: north, east, ...),
  * whether that exit is locked, and where it leads.
  */
-class Exit {
+class Exit : public Lockable {
 private:
     string direction;
-    // Keeps track of keys used to unlock this exit and the unlock status
-    vector<function<bool()>> lockStatusFunctions;
     Room* destination;
 public:
     Exit(string direction, Room* destination, vector<function<bool()>> lockStatusFunctions = {}) :
-        direction(direction), destination(destination), lockStatusFunctions(lockStatusFunctions) {
-    }
+        Lockable(lockStatusFunctions), direction(direction), destination(destination) {}
 
     string getDirection() const { return direction; }
     Room* getDestination() const { return destination; }
-    bool isLocked() const;
-    void addLock(function<bool()> lock) { lockStatusFunctions.push_back(lock); };
 };
