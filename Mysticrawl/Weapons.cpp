6fc187c563c4616d71d42dc9b58f37c588ac2ebb
CoachMac44 <mikebaah_212@yahoo.com>
2025-11-16 21:35:16 -0500

    START5-73 Defending incoming damage decreased by 2

--- Mysticrawl/Weapons.cpp
+++ Mysticrawl/Weapons.cpp
@@ -1,66 +1,33 @@
 #include "Weapons.h"
 #include "Random.h"
-#include <random>
-#include <chrono>
-#include <string>
-#include <algorithm> 
+
+
 
 using namespace GameItems;
 
-Weapon::Weapon(WeaponType t, int p) : type_(t), power_(std::max(0, p)), equipped_(false) {}
+#include "Weapons.h"
 
-WeaponType Weapon::type() const noexcept 
+Weapon::Weapon(WeaponType type, int power)
+    : type_(type), power_(power)
+{
+}
+
+WeaponType Weapon::getType() const
 {
     return type_;
 }
 
-std::string Weapon::name() const 
-{
-    return (type_ == WeaponType::Sword) ? "Sword" : "Staff";
-}
-
-std::string Weapon::description() const
-{
-    if (type_ == WeaponType::Sword)
-    {
-        return "A sword suited for physical strikes.";
-    }
-
-    else
-    {
-        return "A staff that channels immense force.";
-    }
-}
-
-int Weapon::power() const noexcept 
+int Weapon::getPower() const
 {
     return power_;
 }
 
-bool Weapon::isEquipped() const noexcept 
+std::string Weapon::getName() const
 {
-    return equipped_;
+    switch (type_)
+    {
+    case WeaponType::Sword: return "Sword";
+    case WeaponType::Staff: return "Staff";
+    }
 }
 
-void Weapon::equip() noexcept 
-{
-    equipped_ = true;
-}
-
-void Weapon::unequip() noexcept 
-{
-    equipped_ = false;
-}
-
-void Weapon::setPower(int p) noexcept 
-{
-    power_ = std::max(0, p);
-}
-
-int Weapon::attackRoll() const 
-{
-    if (type_ == WeaponType::Sword) 
-        return Random::GetInstance().randInt(std::max(1, power_ - 1), power_ + 3);
-    else 
-        return Random::GetInstance().randInt(std::max(1, power_ - 2), power_ + 2);
-}
--- Mysticrawl/Weapons.h
+++ Mysticrawl/Weapons.h
@@ -8,40 +8,15 @@ namespace GameItems {
         Sword, Staff 
     };
 
-    class Weapon 
+    class Weapon
     {
     public:
-        explicit Weapon(WeaponType type, int power = 5);
+        Weapon(WeaponType type, int power);
+        WeaponType getType()const;
+        int getPower() const;
+        string getName() const;
 
-        WeaponType type()       const noexcept;
-        std::string name()       const;        // "Sword" or "Staff"
-        std::string description() const;       // short text about the weapon
-        int power()      const noexcept;
-        bool isEquipped() const noexcept;
-
-        // User interactions
-        void equip()   noexcept;   // mark as equipped
-        void unequip() noexcept;   // mark as unequipped
-        void setPower(int p) noexcept;
-
-        // Returns a random hit value based on power
-        int attackRoll() const;
-
-        private:
+    private:
         WeaponType type_;
-        int        power_;
-        bool       equipped_;
+        int power_;
     };
-
-    inline Weapon MakeSword(int power = 8) 
-    {
-        return Weapon(WeaponType::Sword, power);
-    }
-    inline Weapon MakeStaff(int power = 5) 
-    {
-        return Weapon(WeaponType::Staff, power); 
-    }
-
-}  
-
-
